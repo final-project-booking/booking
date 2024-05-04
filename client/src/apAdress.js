08e2ae4825b60b81934c38ac28546bd2aac09afb
omarmrad01 <mardomar36@gmail.com>
2024-05-04 16:01:20 +0100

    omar

---
+++ client/src/apAdress.js
@@ -0,0 +1,3 @@
+export const AP_ADRESS="192.168.11.227"
+export const cloud_name="dockwpvkl"
+export const  preset="psocp6cg"
---
+++ client/src/component/detail/Succes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default function Succes() {
+  return (
+   <View>
+    <Text>Reservation successfully</Text>
+    <Text>Thank you for your trust</Text>
+    
+   </View>
+  )
+}
