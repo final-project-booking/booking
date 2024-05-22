25a98397e39e61c80cb7da7f5fdf2c3a7153de0e
ayda-fersi <154595114+ayda-fersi@users.noreply.github.com>
2024-05-22 13:22:48 +0100

    done

--- client/screens/Nav.js
+++ client/screens/Nav.js
@@ -10,7 +10,7 @@ import UserProfile from "../src/component/UserProfile/UserProfile"
 import EditProfile from '../src/component/editprofile/EditProfile';
 import Home from "../src/component/HomePage/Home"
 import OwnerProfile from "../src/component/OwnerProfile/OwnerProfile"
-// import AppFace from "../src/component/AppFace/AppFace.js";
+import AppFace from "../src/component/AppFace/AppFace.js";
 import Favorites from '../src/component/Detail/FavoriteCart'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import IconFa from 'react-native-vector-icons/FontAwesome';
@@ -31,9 +31,6 @@ import ReservationProfile from '../src/component/Detail/ReservationProfile.js';
 import Migrations from "../src/component/OwnerProfile/Migrations.js"
 import Migrations2 from "../src/component/OwnerProfile/Migration2.js"
 import Chhat from './Chhat.js';
-import Payment from '../src/component/Payment/Payment.js'
-import App from '../App.js';
-import CodeQR from '../src/component/AboutUs/CodeQR';
 import Map from "../src/component/Map/Map.js"
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -64,12 +61,10 @@ function Nav({Navigation}) {
     <NavigationContainer>
 
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-        <Stack.Screen name="Map" component={Map} options={{ headerShown: false }}/>
-        
-      <Stack.Screen name="Calander" component={Calander} options={{ headerShown: false }}/>
         <Stack.Screen name="Onboarding"  component={Onboarding}  options={{ headerShown: false }} />
-        <Stack.Screen name="CodeQR" component={CodeQR} options={{ headerShown: false }}/>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+         <Stack.Screen name="Map" component={Map} options={{ headerShown: false }}/> 
+
         <Stack.Screen name="ReservationProfile" component={ReservationProfile} options={{ headerShown: false }}/>
         <Stack.Screen name="Favorites"  component={Favorites}  options={{ headerShown: false }} />  
         <Stack.Screen name="AllHotels" component={AllHotels} options={{ headerShown: false }}/>
@@ -79,8 +74,6 @@ function Nav({Navigation}) {
         <Stack.Screen name="HotelProfile" component={HotelProfile} options={{ headerShown: false }}/>
         <Stack.Screen name="Login"  component={Login}  options={{ headerShown: false }} />
         <Stack.Screen name="ChooseGategory" component={ChooseGategory} options={{ headerShown: false }}/>
-        <Stack.Screen name="Payment" component={Payment} options={{ headerShown: false }}/>
-    
 
 
        
@@ -94,8 +87,9 @@ function Nav({Navigation}) {
        
         <Stack.Screen name="Detail" component={Detail} options={{ headerShown: false }}/>
     
+        <Stack.Screen name="Calander" component={Calander} options={{ headerShown: false }}/>
    
-        {/* <Stack.Screen name="AppFace"  component={AppFace} options={{ headerShown: false }}/> */}
+        <Stack.Screen name="AppFace"  component={AppFace} options={{ headerShown: false }}/>
         <Stack.Screen name="TabNavigator"  component={TabNavigator}  options={{ headerShown: false }} />    
  
 
